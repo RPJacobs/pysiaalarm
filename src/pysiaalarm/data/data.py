d695ca6d00fe6b02e93e0a622b74e848ad306afe
Ralph <ralph.jacobs@syca.nl>
2025-01-06 17:07:26 +0100

    voskuh fix blocking call

--- src/pysiaalarm/data/data.py
+++ src/pysiaalarm/data/data.py
@@ -5,6 +5,7 @@ import json
 from dataclasses import dataclass
 
 import pkg_resources
+import asyncio
 
 FILE_SIA_CODES = "sia_codes.json"
 FILE_XDATA = "xdata.json"
@@ -38,12 +39,17 @@ def _load_data(file: str) -> dict:
     stream = pkg_resources.resource_stream(__name__, file)
     return json.load(stream)
 
+async def _load_file(file:str) -> dict:
+    loop = asyncio.get_running_loop()
+    result = await loop.run_in_executor(None, _load_data, file)
+    return result
 
 def _load_sia_codes() -> dict[str, SIACode]:
     """Alias for loading sia codes file."""
-    data = _load_data(FILE_SIA_CODES)
+    task = asyncio.create_task(_load_file(FILE_SIA_CODES))
+    asyncio.get_running_loop().run_until_complete(task)
+    data = task.result()
     return {key: SIACode(**value) for (key, value) in data.items()}
-
 
 def _load_xdata() -> dict[str, SIAXData]:
     """Alias for loading xdata file."""
